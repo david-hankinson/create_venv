6ec03bea855a41e72d2baeb20c9f7fd6f869db96
David Hankinson <davidmhankinson@outlook.com>
2024-05-29 10:20:51 +0100

    feat: start of user input

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+# create_env - A simple interactive tool to create python virtual environments
+
 ## Usage
 
 `pip install -e .`
-`python3 create_venv`a
+`python3 create_venv`
+
--- create_venv/create_venv.py
+++ create_venv/create_venv.py
@@ -1,10 +1,8 @@
 import subprocess
-import pdb
 
 def create_venv():
-    print(locals())
-    subprocess.run(["python3", "-m", "venv", ".venv"])
-
-    print("Virtual environment created. Activate it with: source .venv/bin/activate")
+    venv_name = input("Enter the name of the virtual environment (default: .venv): ") or ".venv"
+    subprocess.run(["python3", "-m", "venv", venv_name])
+    print(f"Virtual environment '{venv_name}' created. Activate it with: source {venv_name}/bin/activate")
 
 create_venv()
