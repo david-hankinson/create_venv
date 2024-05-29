0c724d2cfa045cd613a694bdd7ddca32f2ec5e12
David Hankinson <davidmhankinson@outlook.com>
2024-05-29 13:13:46 +0100

    feat: copy to clipboard if yes

--- create_venv/create_venv.py
+++ create_venv/create_venv.py
@@ -1,6 +1,12 @@
 import subprocess
+import pyperclip
 
 def create_venv():
     venv_name = input("Enter the name of the virtual environment (default: .venv): ") or ".venv"
     subprocess.run(["python3", "-m", "venv", venv_name])
     print(f"Virtual environment '{venv_name}' created. Activate it with: source {venv_name}/bin/activate")
+
+    copy_to_clipboard = input("Copy 'source {venv_name}/bin/activate' command to clipboard? (default: yes) [yes/no]: ").lower() or "yes"
+    if copy_to_clipboard == "yes":
+        pyperclip.copy(f"source {venv_name}/bin/activate")
+        print("Command copied to clipboard.")
--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="create_venv",
-    version="0.1",
+    name='create_venv',
+    version='0.1',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'create_venv = create_venv:create_venv',  # This allows the function to be run as a command line script
+        ],
+    },
+    install_requires=[
+        # Add any dependencies here
+    ],
+    author='David Hankinson',
+    author_email='davidmhankinson@outlook.com',
+    description='A simple interactive virtual environment creator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/david-hankinson/create_venv',
 )
