e551a13b01931bbbce02800224b6ba1bad34ff52
David Hankinson <davidmhankinson@outlook.com>
2024-05-29 11:27:05 +0100

    chore: cleanup

--- create_venv/create_venv.py
+++ create_venv/create_venv.py
@@ -4,5 +4,3 @@ def create_venv():
     venv_name = input("Enter the name of the virtual environment (default: .venv): ") or ".venv"
     subprocess.run(["python3", "-m", "venv", venv_name])
     print(f"Virtual environment '{venv_name}' created. Activate it with: source {venv_name}/bin/activate")
-
-create_venv()
